1f9ea357b50e21ab444c8332a354c5e8beb6fbb7
chinmayeetapaskar <31750696+chinmayeetapaskar@users.noreply.github.com>
2019-05-30 12:48:39 -0700

    Update AuthenticationServiceApplication.java

--- authentication-service/src/main/java/com/twilio/auth/authenticationservice/AuthenticationServiceApplication.java
+++ authentication-service/src/main/java/com/twilio/auth/authenticationservice/AuthenticationServiceApplication.java
@@ -2,10 +2,10 @@ package com.twilio.auth.authenticationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-
-//@EnableEurekaClient
+@EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class AuthenticationServiceApplication {
 
